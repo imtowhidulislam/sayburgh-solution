3009a4388cbdf43462acc859b7decbcce6f6fa7e
towhid <niamaaktar@gmail.com>
2022-10-03 22:28:10 +0600

    Employeeuser Data updated

--- src/components/EmployeeForm.vue
+++ src/components/EmployeeForm.vue
@@ -1,91 +1,151 @@
 <template>
-    <validation-observer
-      ref="observer"
-      v-slot="{ invalid }"
-      max-width="500px"
+  <div class="d-flex items-center justify-center">
+    <form class="form_container">
+    <v-text-field
+      v-model="name"
+      :error-messages="nameErrors"
+      :counter="20"
+      label="Name"
+      required
+      @input="$v.name.$touch()"
+      @blur="$v.name.$touch()"
+    ></v-text-field>
+    <v-text-field
+      v-model="email"
+      :error-messages="emailErrors"
+      label="E-mail"
+      required
+      @input="$v.email.$touch()"
+      @blur="$v.email.$touch()"
+    ></v-text-field>
+    <v-text-field
+      v-model="phone"
+      :error-messages="emailErrors"
+      label="Phone"
+      required
+      @input="$v.phoen.$touch()"
+      @blur="$v.phone.$touch()"
+    ></v-text-field>
+    
+   <!--  <v-checkbox
+      v-model="checkbox"
+      :error-messages="checkboxErrors"
+      label="Do you agree?"
+      required
+      @change="$v.checkbox.$touch()"
+      @blur="$v.checkbox.$touch()"
+    ></v-checkbox> -->
+
+    <v-btn
+      class="mr-4"
+      @click="submit"
     >
-      <form @submit.prevent="submit">
-        <validation-provider
-          v-slot="{ errors }"
-          name="Name"
-          rules="required|max:10"
-        >
-          <v-text-field
-            v-model="name"
-            :counter="10"
-            :error-messages="errors"
-            label="Name"
-            required
-          ></v-text-field>
-        </validation-provider>
-        <validation-provider
-          v-slot="{ errors }"
-          name="phoneNumber"
-          :rules="{
-            required: true,
-            digits: 7,
-            regex: '^(71|72|74|76|81|82|84|85|86|87|88|89)\\d{5}$'
-          }"
-        >
-          <v-text-field
-            v-model="phoneNumber"
-            :counter="7"
-            :error-messages="errors"
-            label="Phone Number"
-            required
-          ></v-text-field>
-        </validation-provider>
-        <validation-provider
-          v-slot="{ errors }"
-          name="email"
-          rules="required|email"
-        >
-          <v-text-field
-            v-model="email"
-            :error-messages="errors"
-            label="E-mail"
-            required
-          ></v-text-field>
-        </validation-provider>
-        <!-- <validation-provider
-          v-slot="{ errors }"
-          name="select"
-          rules="required"
-        >
-          <v-select
-            v-model="select"
-            :items="items"
-            :error-messages="errors"
-            label="Select"
-            data-vv-name="select"
-            required
-          ></v-select>
-        </validation-provider> -->
-        <validation-provider
-          v-slot="{ errors }"
-          rules="required"
-          name="checkbox"
-        >
-          <v-checkbox
-            v-model="checkbox"
-            :error-messages="errors"
-            value="1"
-            label="Option"
-            type="checkbox"
-            required
-          ></v-checkbox>
-        </validation-provider>
+      submit
+    </v-btn>
+    <!-- <v-btn @click="clear">
+      clear
+    </v-btn> -->
+  </form>
   
-        <v-btn
-          class="mr-4"
-          type="submit"
-          :disabled="invalid"
-        >
-          submit
-        </v-btn>
-        <!-- <v-btn @click="clear">
-          clear
-        </v-btn> -->
-      </form>
-    </validation-observer>
+  </div>
+   
+  
   </template>
+
+  <script>
+    import { validationMixin } from 'vuelidate'
+  import { required, maxLength, email } from 'vuelidate/lib/validators'
+
+  export default {
+    mixins: [validationMixin],
+
+    validations: {
+      name: { required, maxLength: maxLength(10) },
+      email: { required, email },
+      select: { required },
+      /* checkbox: {
+        checked (val) {
+          return val
+        },
+      }, */
+    },
+
+    data: () => ({
+      name: '',
+      email: '',
+      phone: '',
+      // checkbox: false,
+
+      Employee : []
+    }),
+
+    computed: {
+     /*  checkboxErrors () {
+        const errors = []
+        if (!this.$v.checkbox.$dirty) return errors
+        !this.$v.checkbox.checked && errors.push('You must agree to continue!')
+        return errors
+      }, */
+   /*    selectErrors () {
+        const errors = []
+        if (!this.$v.select.$dirty) return errors
+        !this.$v.select.required && errors.push('Item is required')
+        return errors
+      }, */
+      
+      nameErrors () {
+        const errors = []
+        if (!this.$v.name.$dirty) return errors
+        !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
+        !this.$v.name.required && errors.push('Name is required.')
+        return errors
+      },
+      emailErrors () {
+        const errors = []
+        if (!this.$v.email.$dirty) return errors
+        !this.$v.email.email && errors.push('Must be valid e-mail')
+        !this.$v.email.required && errors.push('E-mail is required')
+        return errors
+      },
+    },
+
+    methods: {
+      submit () {
+        console.log('sumbit');
+        const newEmployee = {
+          name : this.name,
+          email : this.email,
+          phone : this.phone,
+          id: new Date().getTime().toString()
+        }
+        this.Employee = [...this.Employee, newEmployee],
+        this.name = "",
+        this.email = "",
+        this.phone = ""
+        this.$emit('MyEmployee', this.Employee)
+      },
+      
+    },
+    mounted(){
+      console.log('App Mounted');
+        if (localStorage.getItem('EmployeeData'))
+            this.Employee = JSON.parse(localStorage.getItem('EmployeeData'));
+    },
+    watch:{
+      Employee:{
+        handler(){
+          localStorage.setItem('EmployeeData', JSON.stringify(this.Employee))
+        },
+        deep:true
+      }
+    }
+  }
+  </script>
+  <style scoped> 
+  .form_container{
+    /* max-width: 800px; */
+    background-color: #ffaacc;
+    padding: 2rem;
+    border-radius: .5rem;
+  }
+  </style>
--- src/views/EmployeeUser.vue
+++ src/views/EmployeeUser.vue
@@ -1,9 +1,82 @@
 <template>
-    <h2>This is Employee Page</h2>
-</template>
+    <div>
+        <h2>This is Employee Page</h2>
+        <EmployeeForm @MyEmployee="getEmployee($data)"/>
 
+        <v-container xm12 sm6 md4 lg3 class="my-12" v-for="em in ourEmployee" :key="em.id">
+            <v-card
+                class="mx-auto"
+                max-width="400"
+            >
+                <v-container class="d-flex justify-center">
+
+                    <v-avatar width="100" height="100">
+                        
+                    <v-img
+                    class="white--text align-end"
+                    height="200px"
+                    src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                    >
+                    <!-- <v-card-title>Top 10 Australian beaches</v-card-title> -->
+                    </v-img>
+                </v-avatar>
+                </v-container>
+
+                <v-card-subtitle class="pb-0 text-center">
+                {{em.name}}
+                </v-card-subtitle>
+
+                <v-card-text class="text--primary text-center">
+                <div>{{em.email}}</div>
+
+                <div>{{em.phone}}</div>
+                </v-card-text>
+
+                <v-card-actions class="d-flex justify-center">
+                    <v-btn
+                        color="orange"
+                        text
+                    >
+                        Edit
+                        <v-icon>mdi-lead-pencil</v-icon>
+                    </v-btn>
+
+                    <v-btn
+                        color="orange"
+                        text
+                    >
+                        delete
+                        <v-icon>mdi-archive-cancel</v-icon>
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-container>
+
+    </div>
+</template>
+<!-- <div v-for="em in Employee" :key="em.phone">
+    <h2>{{em.name}}</h2>
+    <h2>{{em.email}}</h2>
+    <h2>{{em.phone}}</h2>
+  </div> -->
 <script>
-    
+    import EmployeeForm from "@/components/EmployeeForm.vue";
+    export default {
+        components : {
+            EmployeeForm
+        },
+        data() {
+            return {
+                ourEmployee :[],
+                // console.log(ourEmployee)
+            }
+        },
+        methods: {
+            getEmployee(data) {
+                this.ourEmployee = [...this.ourEmployee, data]
+            }
+        }
+    }
 </script>
 <style scoped>
     
